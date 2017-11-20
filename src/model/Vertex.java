8185e50c7cbf0ded7e8c88ba9e66b716efec294d
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2017-11-20 17:18:14 +0100

    Ajouet et complétion de Vertex

--- src/model/Vertex.java
+++ src/model/Vertex.java
@@ -3,5 +3,47 @@ package model;
 public class Vertex 
 {
 
-	private int x, y;
+	private int _x, _y, _nbr;
+
+	public Vertex (int x, int y, int nbr)
+	{
+		_x = x;
+		_y = y;
+		_nbr = nbr;
+	}
+	
+	public int getNbr() 
+	{
+		return _nbr;
+	}
+
+	public void setNbr(int nbr) {
+		_nbr = nbr;
+	}
+
+	public int getX() 
+	{
+		return _x;
+	}
+
+	public void setX(int x) 
+	{
+		_x = x;
+	}
+
+	public int getY() 
+	{
+		return _y;
+	}
+
+	public void setY(int y) 
+	{
+		_y = y;
+	}
+	
+	public Boolean compareTo (Vertex v)
+	{
+		
+		return (v.getX() == _x && v.getY() == _y);
+	}
 }
