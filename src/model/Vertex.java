45578cb3a4a1540116fc7cc2dcae0022ae790bcf
Okan SOYTURK <okasoyturk@emi.u-bordeaux1.fr>
2017-11-20 18:01:46 +0100

    vertex

--- src/model/Vertex.java
+++ src/model/Vertex.java
@@ -1,46 +1,51 @@
 package model;
 
-public class Vertex {
+public class Vertex 
+{
 
 	private int _x, _y, _nbr;
 
-	public Vertex(int x, int y, int nbr) {
+	public Vertex (int x, int y, int nbr)
+	{
 		_x = x;
 		_y = y;
 		_nbr = nbr;
 	}
-
-	public int getNbr() {
+	
+	public int getNbr() 
+	{
 		return _nbr;
 	}
 
-	public void setNbr(int nbr) {
+	public void setNbr(int nbr) 
+	{
 		_nbr = nbr;
 	}
 
-	public int getX() {
+	public int getX() 
+	{
 		return _x;
 	}
 
-	public void setX(int x) {
+	public void setX(int x) 
+	{
 		_x = x;
 	}
 
-	public int getY() {
+	public int getY() 
+	{
 		return _y;
 	}
 
-	public void setY(int y) {
+	public void setY(int y) 
+	{
 		_y = y;
 	}
-
-	public int compareTo(Vertex v) {
+	
+	public int compareTo (Vertex v)
+	{
 		if (v.getX() == _x && v.getY() == _y && v.getNbr() == _nbr)
 			return 0;
 		return 1;
-	}
-	
-	public int inBorders() {
-		
 	}
 }
