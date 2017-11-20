dd0d7c04d2ae2c8115db40ef2eb2cb14f823fb90
Jeffrey Seutin <jefseutin@emi.u-bordeaux1.fr>
2017-11-20 17:11:51 +0100

    ajout Edge.java

---
+++ src/model/Edge.java
@@ -0,0 +1,44 @@
+package model;
+
+import org.jgrapht.graph.DefaultEdge;
+
+public class Edge extends DefaultEdge implements Comparable<Edge>{
+	public enum Type{
+		OPENED_DOOR,
+		CLOSED_DOOR,
+		CORRIDOR;
+	};
+	
+	private Type type;
+	
+	public Edge(Type type){
+		super();
+		this.type = Type.CORRIDOR;
+	}
+	
+	public Vertex getSource() {
+		return (Vertex) super.getSource();
+	}
+	
+	public Vertex getTarget() {
+		return (Vertex) super.getTarget();
+	}
+	
+	public Type getType() {
+		return type;
+	}
+	
+	public void setType(Type type) {
+		this.type = type;
+	}
+	
+	@Override
+	public int compareTo(Edge o) {
+		int source = this.getSource().compareTo((o).getSource());
+		if (source!=0)
+			return source;
+		else {
+			return this.getTarget().compareTo((o).getTarget());
+		}
+	}
+}
