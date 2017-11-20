1d6138a3d2e1a9c6a9e3138908c68dc9f7113fa6
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2017-11-20 18:09:07 +0100

    rajout de nbr de tarass

--- src/model/Vertex.java
+++ src/model/Vertex.java
@@ -2,12 +2,23 @@ package model;
 
 public class Vertex {
 
-	private int _x, _y;
+	private int _x, _y, _nbr;
 
-	public Vertex (int x, int y)
+	public int getNbr()
+	{
+		return _nbr;
+	}
+
+	public void setNbr(int nbr) 
+	{
+		_nbr = nbr;
+	}
+
+	public Vertex (int x, int y, int nbr)
 	{
 		_x = x;
 		_y = y;
+		_nbr = nbr;
 	}
 	
 	public int getX()
@@ -29,12 +40,16 @@ public class Vertex {
 	
 	public int compareTo (Vertex v)
 	{
-		if (v.getX() == _x && v.getY() == _y)
+		if (v.getX() == _x && v.getY() == _y && _nbr == v.getNbr())
 			return 0;
 		return 1;
 	}
 	
-	public int inBorders() {
-		
+	public int inBorders (int width, int height)
+	{
+		if (_x <= width && _x >= 0)
+			if (_y <= height && _y >= 0)
+				return 0;
+		return 1;
 	}
 }
