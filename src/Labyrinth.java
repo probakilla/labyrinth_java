135c5229ea1e8483b08b56ae6d013b129b949691
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2017-11-27 14:21:15 +0100

    Indentation automatique

--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -1,35 +1,40 @@
+
 import controller.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Labyrinth extends Application 
+/**
+ * This class is used to start the program.
+ *
+ * @author Java Group
+ */
+public class Labyrinth extends Application
 {
 
-	static Controller _controller;
+    static Controller _controller;
 
-	public static void main(String[] args) 
-	{
-		launch();
-	}
+    public static void main(String[] args)
+    {
+        launch();
+    }
 
-	@Override
-	public void start(Stage primaryStage) 
-	{
-		try 
-		{
-			_controller = Controller.getInstance();
-			_controller.start(primaryStage);
-		} 
-		catch (Exception e) 
-		{
-			System.out.println(e.getMessage());
-		}
-	}
+    @Override
+    public void start(Stage primaryStage)
+    {
+        try
+        {
+            _controller = Controller.getInstance();
+            _controller.start(primaryStage);
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 
-	@Override
-	public void stop() 
-	{
-		System.exit(0);
-	}
+    @Override
+    public void stop()
+    {
+        System.exit(0);
+    }
 
 }
